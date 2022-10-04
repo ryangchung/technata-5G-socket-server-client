80248ac628a7dbcf671b3f3cfd968647e7ca34bc
Ryan C <87027981+ryangchung@users.noreply.github.com>
2022-10-04 15:32:16 -0400

    added id system

--- client.py
+++ client.py
@@ -1,34 +1,50 @@
 import socket
 
-host = "127.0.0.1"
-port = 9999
-client_total_power = 0
 
-def send_message(host, port, message):
+def send_message(host, port, message, sensor_id):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        s.sendall(bytes(message, 'utf-8'))
+        s.sendall(bytes(f"{message},{sensor_id}", "utf-8"))
         return s.recv(1024).decode()
 
-while True:
-    try:
-        # Checking user input - Should not have any errors getting passed into the server
-        user_input = input("Input action (ex: +23W): ")
-        command = user_input[0]
-        quantity = int(user_input[1:-1])
-        if command == "+":
-            client_total_power += quantity
-            print("Local power:", client_total_power, "W")
-            print(send_message(host, port, user_input), "\n")
-        elif command == "-":
-            if client_total_power - quantity < 0:
-                raise Exception
-            else:
-                client_total_power -= quantity
+
+def main():
+    host = "127.0.0.1"
+    port = 9999
+
+    client_total_power = 0
+
+    while True:
+        try:
+            # ID has to be a number
+            sensor_id = input("Sensor ID: ")
+            break
+        except Exception:
+            print("Incorrect ID.")
+
+    while True:
+        try:
+            # Checking user input - Should not have any errors getting passed into the server
+            user_input = input("Input action (ex: +23W): ")
+            command = user_input[0]
+            quantity = int(user_input[1:-1])
+            if command == "+" and user_input[-1] == "W":
+                client_total_power += quantity
                 print("Local power:", client_total_power, "W")
-                print(send_message(host, port, user_input))
-        else:
-            raise Exception
-    except Exception:
-        print("Invalid command")
-        continue
+                print(send_message(host, port, user_input, sensor_id))
+            elif command == "-" and user_input[-1] == "W":
+                if client_total_power - quantity < 0:
+                    raise ValueError
+                else:
+                    client_total_power -= quantity
+                    print("Local power:", client_total_power, "W")
+                    print(send_message(host, port, user_input, sensor_id))
+            else:
+                raise ValueError
+        except ValueError:
+            print("Invalid command")
+            continue
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -1,22 +1,49 @@
 import socket
 
-total_needed_power = 0
 
-while True:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind(("127.0.0.1", 9999))  # localhost, port above 1023
-        s.listen()
-        connection, address = s.accept()
-        with connection:
-            print("Connected by", address)
-            data = connection.recv(1024)
-            decoded_data = data.decode()
-            print("Received request of", decoded_data)
+def main():
+    host = "127.0.0.1"
+    port = 9999
 
-            if decoded_data[0] == "+":
-                total_needed_power += int(decoded_data[1:-1])
-            else:
-                total_needed_power -= int(decoded_data[1:-1])
-            print("Total power is now", total_needed_power, "W")
+    total_needed_power = 0
+    current_usage = {}
 
-            connection.send(bytes(f"Request of {decoded_data} was completed", 'utf-8'))
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((host, port))  # localhost, port above 1023
+            s.listen()
+            connection, address = s.accept()
+            # print("Connected by", address)
+            with connection:
+                decoded_data = connection.recv(1024).decode().split(",")
+                power_quantity, sensor_id = int(decoded_data[0][1:-1]), decoded_data[1]
+                print(f"Received request of {decoded_data[0]} by ID: {sensor_id}")
+
+                if sensor_id not in current_usage.keys():
+                    current_usage[sensor_id] = 0
+                    current_usage = dict(sorted(current_usage.items()))
+
+                if decoded_data[0][0] == "+":
+                    total_needed_power += power_quantity
+                    current_usage[sensor_id] += power_quantity
+                else:
+                    total_needed_power -= power_quantity
+                    current_usage[sensor_id] -= power_quantity
+
+                if current_usage[sensor_id] == 0:
+                    del current_usage[sensor_id]
+                    current_usage = dict(sorted(current_usage.items()))
+
+                print("Total power is now", total_needed_power, "W")
+                print("Breakdown of power usage:", current_usage)
+
+                connection.send(
+                    bytes(
+                        f"Request of {decoded_data[0]} by ID: {sensor_id} was completed",
+                        "utf-8",
+                    )
+                )
+
+
+if __name__ == "__main__":
+    main()
