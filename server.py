5f7821175157271ee6a86371ac63098d0d2abeb1
Ryan C <87027981+ryangchung@users.noreply.github.com>
2022-10-05 12:24:27 -0400

    private properties

--- server.py
+++ server.py
@@ -2,25 +2,25 @@ import socket
 
 
 class Server:
-    total_needed_power = 0
-    current_usage = {}
+    __total_needed_power = 0
+    __current_usage: dict = {}
 
     # Given an action, return the new power that this sensor is using
     def modify_power_draw(self, action, power_quantity, sensor_id):
         if action == "+":
-            self.total_needed_power += power_quantity
-            self.current_usage[sensor_id] += power_quantity
+            self.__total_needed_power += power_quantity
+            self.__current_usage[sensor_id] += power_quantity
         else:
-            self.total_needed_power -= power_quantity
-            self.current_usage[sensor_id] -= power_quantity
+            self.__total_needed_power -= power_quantity
+            self.__current_usage[sensor_id] -= power_quantity
 
     # Returned an ordered dict based off the key names
     def order_dict(self):
-        self.current_usage = dict(sorted(self.current_usage.items()))
+        self.__current_usage = dict(sorted(self.__current_usage.items()))
 
     # Remove an element from a dict and return a new sorted dict
     def remove_from_dict(self, sensor_id):
-        del self.current_usage[sensor_id]
+        del self.__current_usage[sensor_id]
         self.order_dict()
 
     def main(self):
@@ -39,15 +39,15 @@ class Server:
 
                     # If the sensor is shutting down
                     if decoded_data[0] == "exit":
-                        # If it exists, then remove from current_usage and update total_needed_power
+                        # If it exists, then remove from __current_usage and update __total_needed_power
                         print(
                             f"Received request of {decoded_data[0]} by ID: {sensor_id}"
                         )
-                        if sensor_id in self.current_usage.keys():
-                            self.current_usage = self.remove_from_dict(sensor_id)
-                            self.total_needed_power = sum(self.current_usage.values())
+                        if sensor_id in self.__current_usage.keys():
+                            self.__current_usage = self.remove_from_dict(sensor_id)
+                            self.__total_needed_power = sum(self.__current_usage.values())
                             print("Removed ID:", sensor_id, "for exiting.")
-                            print("Breakdown of power usage:", self.current_usage)
+                            print("Breakdown of power usage:", self.__current_usage)
                         else:
                             connection.send(bytes("Nothing to remove.", "utf-8"))
 
@@ -60,22 +60,22 @@ class Server:
                         power_quantity = int(decoded_data[0][1:-1])
 
                         # Creates a dict entry if the sensor does not have one already
-                        if sensor_id not in self.current_usage.keys():
-                            self.current_usage[sensor_id] = 0
+                        if sensor_id not in self.__current_usage.keys():
+                            self.__current_usage[sensor_id] = 0
                             self.order_dict()
 
-                        # Updates the total_needed_power as well as the power draw for the sensor in the dict
+                        # Updates the __total_needed_power as well as the power draw for the sensor in the dict
                         self.modify_power_draw(
                             decoded_data[0][0], power_quantity, sensor_id
                         )
 
                         # If it is not currently using power, remove it from the dict
-                        if self.current_usage[sensor_id] == 0:
+                        if self.__current_usage[sensor_id] == 0:
                             self.remove_from_dict(sensor_id)
 
                         # Output
-                        print("Total power is now", self.total_needed_power, "W")
-                        print("Breakdown of power usage:", self.current_usage)
+                        print("Total power is now", self.__total_needed_power, "W")
+                        print("Breakdown of power usage:", self.__current_usage)
 
                         # Send connection success
                         connection.send(
